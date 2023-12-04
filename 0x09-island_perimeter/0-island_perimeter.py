15687ec5c2f9ce39c573449c3670e985cfe5a059
ggakila <gisorehaggai@gmail.com>
2023-12-04 04:19:00 +0300

    final commits

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -4,20 +4,41 @@
 
 def island_perimeter(grid):
     """returns the perimeter of the island described in grid"""
-    perimeter = 0
+    count = 0
     for i in range(len(grid)):
         for j in range(len(grid[i])):
             if grid[i][j] == 1:
                 # up
-                if i == 0 or grid[i - 1][j] == 0:
-                    perimeter += 1
+                if i - 1 < 0:
+                    count += 1
+                else:
+                    try:
+                        if grid[i - 1][j] == 0 or i - 1 < 0:
+                            count += 1
+                    except:
+                        pass
                 # down
-                if i == len(grid) - 1 or grid[i + 1][j] == 0:
-                    perimeter += 1
-                # left
-                if j == 0 or grid[i][j - 1] == 0:
-                    perimeter += 1
+                if i + 1 > len(grid) - 1:
+                    count += 1
+                else:
+                    try:
+                        if grid[i + 1][j] == 0:
+                            count += 1
+                    except:
+                        pass
                 # right
-                if j == len(grid[i]) - 1 or grid[i][j + 1] == 0:
-                    perimeter += 1
-    return perimeter
+                if j + 1 > len(grid[i]) - 1:
+                    count += 1
+                else:
+                    try:
+                        if grid[i][j + 1] == 0:
+                            count += 1
+                    except:
+                        pass
+                # left
+                try:
+                    if grid[i][j - 1] == 0 or j - 1 < 0:
+                        count += 1
+                except:
+                    pass
+    return count

af77295554ab162a0a33c4c45b64f2754f45be88
ggakila <gisorehaggai@gmail.com>
2023-12-11 05:03:01 +0300

    pep8 validation

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,17 +1,18 @@
 #!/usr/bin/python3
 """Module for solving the prime game question."""
 
+
 def isWinner(x, nums):
     """Determine the winner of the prime game."""
     if not nums or x < 1:
         return None
-    largets_num = max(nums)
+    largest_num = max(nums)
 
-    filtered_nums = [True for _ in range(max(largets_num + 1, 2))]
-    for i in range(2, int(pow(largets_num, 0.5)) + 1):
+    filtered_nums = [True for _ in range(max(largest_num + 1, 2))]
+    for i in range(2, int(pow(largest_num, 0.5)) + 1):
         if not filtered_nums[i]:
             continue
-        for j in range(i * i, largets_num + 1, i):
+        for j in range(i * i, largest_num + 1, i):
             filtered_nums[j] = False
     filtered_nums[0] = filtered_nums[1] = False
     y = 0

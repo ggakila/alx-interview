89653500d28564896c771d801295a5f3b85c7239
ggakila <gisorehaggai@gmail.com>
2023-12-11 04:38:40 +0300

    last checks

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,39 +1,29 @@
 #!/usr/bin/python3
-""" Prime game task"""
+""" Module for solving prime game question """
+
 def isWinner(x, nums):
+    """function that checks for the winner"""
     if not nums or x < 1:
         return None
+    max_num = max(nums)
 
-    def is_prime(num):
-        if num < 2:
-            return False
-        for i in range(2, int(num**0.5) + 1):
-            if num % i == 0:
-                return False
-        return True
-
-    def play_game(n):
-        primes = [i for i in range(2, n+1) if is_prime(i)]
-        turn = 0  # 0 for Maria, 1 for Ben
-        while primes:
-            chosen_prime = primes[0]
-            primes = [p for p in primes if p % chosen_prime != 0]
-            turn = 1 - turn  # Switch turns
-        return turn  
-
-    maria_wins = 0
-    ben_wins = 0
-
-    for n in nums:
-        winner = play_game(n)
-        if winner == 0:
-            maria_wins += 1
-        elif winner == 1:
-            ben_wins += 1
-
-    if maria_wins > ben_wins:
-        return "Maria"
-    elif maria_wins < ben_wins:
-        return "Ben"
-    else:
+    my_filter = [True for _ in range(max(max_num + 1, 2))]
+    for i in range(2, int(pow(max_num, 0.5)) + 1):
+        if not my_filter[i]:
+            continue
+        for j in range(i * i, max_num + 1, i):
+            my_filter[j] = False
+    my_filter[0] = my_filter[1] = False
+    y = 0
+    for i in range(len(my_filter)):
+        if my_filter[i]:
+            y += 1
+        my_filter[i] = y
+    player1 = 0
+    for x in nums:
+        player1 += my_filter[x] % 2 == 1
+    if player1 * 2 == len(nums):
         return None
+    if player1 * 2 > len(nums):
+        return "Maria"
+    return "Ben"
